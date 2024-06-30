2c11f850f2958e3b5d9373a3b4e014cbebbd71fd
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-30 12:14:30 +0600

    fixed importing js-cookie

--- frontend/src/api/apiRequest.js
+++ frontend/src/api/apiRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getBaseURL } from "./../utils/baseUrl";
-import { Cookies } from "js-cookie";
+import Cookies from "js-cookie";
 
 const BASE_URL = getBaseURL();
 
